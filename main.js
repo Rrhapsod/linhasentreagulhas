a435b1eeea335fa18329a878f9e50a7049d35b07
Renan Rondon <47695915+Rrhapsod@users.noreply.github.com>
2022-06-02 22:53:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+window.addEventListener("scroll", onScroll);
+
+function onScroll() {
+  showNavOnScroll();
+  showBackToTopButton();
+}
+
+function showNavOnScroll() {
+  if (scrollY == 0) {
+    navigation.classList.remove("scroll");
+  } else {
+    navigation.classList.add("scroll");
+  }
+}
+
+function showBackToTopButton() {
+  if (scrollY > 800) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+function openMenu() {
+  document.body.classList.add("menu-expanded");
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu-expanded");
+}
+
+ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+}).reveal(
+  "#home, #home img, #home .stats, #produtos, #produtos header, #produtos .card, #about, #about header, #about .content, #contact, #contact header, #contact .content"
+);
